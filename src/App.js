a8c0c45476e547aedaa3e76f592ad58fd79564eb
k41205 <giacomocavarretta@gmail.com>
2022-04-13 14:43:29 +0100

    created FirebaseContext component

--- src/App.js
+++ src/App.js
@@ -1,92 +1,22 @@
 import './App.css';
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
 import Footer from './components/UI/Footer';
 import Header from './components/UI/Header';
 import Main from './components/UI/Main';
 import Budget from './components/Budget/Budget';
-import {
-  collection,
-  addDoc,
-  getDocs,
-  updateDoc,
-  arrayUnion,
-} from 'firebase/firestore';
 import { useEffect, useState, useCallback, useContext } from 'react';
 import FirebaseContext from './contexts/firebase-context';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyC2hmOydTEpdmcpoOzzuVLng_j7u76djpo',
-  authDomain: 'neeko-aa020.firebaseapp.com',
-  projectId: 'neeko-aa020',
-  storageBucket: 'neeko-aa020.appspot.com',
-  messagingSenderId: '727544693791',
-  appId: '1:727544693791:web:1fc671ecfca5671ea1012a',
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
-
-const postData = async (type, data) => {
-  if (type === 'container') {
-    try {
-      const docRef = await addDoc(collection(db, 'containers'), {
-        name: data,
-        fields: [],
-      });
-      console.log('Document written with ID: ', docRef.id);
-    } catch (e) {
-      console.error('Error adding document: ', e);
-    }
-  }
-  if (type === 'field') {
-    console.log(data);
-    try {
-      const docRef = await updateDoc(data.ref, {
-        fields: arrayUnion(data),
-      });
-      // console.log('Document updated with ID: ', data.id);
-    } catch (e) {
-      console.error('Error updating document: ', e);
-    }
-  }
-};
-
 function App() {
-  const [containers, setContainers] = useState([]);
   const ctx = useContext(FirebaseContext);
 
-  console.log(ctx.text);
-  ctx.text = 'test';
-  console.log(ctx.text);
-
-  const getData = useCallback(async () => {
-    const querySnapshot = await getDocs(collection(db, 'containers'));
-    // obj.docs[0]._document.data.value.mapValue.fields
-    // console.log(querySnapshot);
-    const containers = querySnapshot.docs.map((doc) => {
-      const data = doc.data();
-      data.ref = doc.ref;
-      data.id = doc.id;
-      return data;
-    });
-    // console.log(containers);
-    setContainers(containers);
-  }, []);
-
-  useEffect(() => {
-    getData();
-  }, [getData]);
+  console.log(ctx);
 
   return (
     <>
       <Header />
-      <button onClick={getData}></button>
+      <button onClick={ctx.getData}></button>
       <Main>
-        <Budget data={containers} onSave={postData} />
+        <Budget data={ctx.containers} onSave={ctx.postData} />
       </Main>
       <Footer />
     </>
--- src/contexts/firebase-context.js
+++ src/contexts/firebase-context.js
@@ -1,12 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+  arrayUnion,
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyC2hmOydTEpdmcpoOzzuVLng_j7u76djpo',
+  authDomain: 'neeko-aa020.firebaseapp.com',
+  projectId: 'neeko-aa020',
+  storageBucket: 'neeko-aa020.appspot.com',
+  messagingSenderId: '727544693791',
+  appId: '1:727544693791:web:1fc671ecfca5671ea1012a',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
 const FirebaseContext = React.createContext({
-  text: '',
+  postData: (type, data) => {},
+  getData: () => {},
 });
 
 export const FirebaseContextProvider = (props) => {
-  const test = 'ciao';
+  const [containers, setContainers] = useState([]);
+
+  const postData = async (type, data) => {
+    if (type === 'container') {
+      try {
+        const docRef = await addDoc(collection(db, 'containers'), {
+          name: data,
+          fields: [],
+        });
+        console.log('Document written with ID: ', docRef.id);
+      } catch (e) {
+        console.error('Error adding document: ', e);
+      }
+    }
+    if (type === 'field') {
+      console.log(data);
+      try {
+        const docRef = await updateDoc(data.ref, {
+          fields: arrayUnion(data),
+        });
+        // console.log('Document updated with ID: ', data.id);
+      } catch (e) {
+        console.error('Error updating document: ', e);
+      }
+    }
+  };
+
+  const getData = async () => {
+    const querySnapshot = await getDocs(collection(db, 'containers'));
+    // obj.docs[0]._document.data.value.mapValue.fields
+    // console.log(querySnapshot);
+    const containers = querySnapshot.docs.map((doc) => {
+      const data = doc.data();
+      data.ref = doc.ref;
+      data.id = doc.id;
+      return data;
+    });
+    // console.log(containers);
+    setContainers(containers);
+  };
+
   return (
-    <FirebaseContext.Provider value={{ text: test }}>
+    <FirebaseContext.Provider value={{ containers, getData, postData }}>
       {props.children}
     </FirebaseContext.Provider>
   );
